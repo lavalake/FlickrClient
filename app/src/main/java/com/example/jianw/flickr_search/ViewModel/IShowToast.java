d695be14b7076d3fe191e4988ac31f8f51add64a
lavalake <jian1.w@samsung>
2018-11-28 14:32:43 -0800

    new interface

---
+++ app/src/main/java/com/example/jianw/flickr_search/ViewModel/IShowToast.java
@@ -0,0 +1,9 @@
+package com.example.jianw.flickr_search.ViewModel;
+
+/**
+ * Created by jian1.w on 11/28/18.
+ */
+
+public  interface IShowToast {
+    public void showToast(final String message);
+}
